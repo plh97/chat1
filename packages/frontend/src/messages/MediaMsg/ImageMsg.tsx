64ee821c27b4c9bad3f2c08c0c240206fbd02420
plh97 <pengliheng111@gmail.com>
2024-10-21 21:15:06 +0800

    fix: replace chrome with chromium in e2e

--- packages/frontend/src/messages/MediaMsg/ImageMsg.tsx
+++ packages/frontend/src/messages/MediaMsg/ImageMsg.tsx
@@ -3,55 +3,31 @@ import { FaDownload } from "react-icons/fa";
 import { ProgressImage } from "./ProgressImage";
 import { IMediaMessage } from "@/interfaces";
 
-const useFixedSize = (
-  mediaMessage?: IMediaMessage,
-  maxWidth = 200,
-  maxHeight = 200
-) => {
-  if (!mediaMessage?.width || !mediaMessage?.height) {
-    return { width: 0, height: 0 };
-  }
-  const rate = mediaMessage.width / mediaMessage.height;
-  let width = 0;
-  let height = 0;
-  if (mediaMessage.width > mediaMessage.height) {
-    width = maxWidth;
-    height = maxHeight / rate;
-  } else {
-    height = maxWidth;
-    width = maxHeight * rate;
-  }
-  return {
-    width,
-    height,
-  };
-};
-
 const PreviewImage = ({
   mediaMessage,
   children,
+  className,
 }: Partial<IMediaMessage> & {
   mediaMessage: IMediaMessage;
   children: JSX.Element;
+  className: string;
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { url, thumbnail } = mediaMessage;
-  // const { width, height } = useFixedSize(mediaMessage, 800, 600);
   return (
     <>
-      {children}
+      <div onClick={onOpen} className={className}>
+        {children}
+      </div>
       <Modal isCentered onClose={onClose} size={"xl"} isOpen={isOpen}>
         <ModalOverlay bg="none" backdropFilter="auto" backdropBlur="2px" />
-        <ModalContent className="flex items-center justify-center !bg-black/0">
+        <ModalContent className="flex items-center justify-center">
           <ModalCloseButton />
           <div className="max-w-[80vw] max-h-[80vh]">
             <Image
               src={url}
               fallbackSrc={thumbnail ?? ""}
-              className="bg-contain bg-no-repeat bg-center object-contain"
-              style={{
-                backgroundImage: `url(${thumbnail})`,
-              }}
+              className="bg-contain bg-no-repeat bg-center object-contain max-w-[70vw] max-h-[70vh]"
             />
           </div>
           <a href={url} target="_blank" rel="noreferrer" download="3223.png">
@@ -72,17 +48,13 @@ const PreviewImage = ({
 
 export const ImageMsg = ({ message }: { message: IMediaMessage }) => {
   const thumbnail = message?.thumbnail;
-  const width = message?.width ?? 0;
-  const height = message?.height ?? 0;
   if (!thumbnail) return <div>Invalid Image</div>;
   return (
-    <div
+    <PreviewImage
       className="overflow-hidden box-content p-2 max-w-[200px] max-h-[200px] cursor-pointer select-none"
-      style={{ height, width }}
+      mediaMessage={message}
     >
-      <PreviewImage mediaMessage={message}>
-        <ProgressImage thumbnail={message.thumbnail} url={message.url} />
-      </PreviewImage>
-    </div>
+      <ProgressImage message={message} />
+    </PreviewImage>
   );
 };
--- packages/frontend/src/messages/MediaMsg/ProgressImage.tsx
+++ packages/frontend/src/messages/MediaMsg/ProgressImage.tsx
@@ -1,35 +1,42 @@
 import { Image } from "@chakra-ui/react";
 import { IMediaMessage } from "core";
 import classNames from "classnames";
+import { CSSProperties } from "react";
 
-export const ProgressImage = ({ thumbnail, url }: Partial<IMediaMessage>) => {
+export const ProgressImage = ({
+  message,
+}: {
+  message: Partial<IMediaMessage>;
+}) => {
+  const { thumbnail, url } = message;
+  const w = message.width!;
+  const h = message.height!;
   const [loading, setLoading] = useState<boolean>(true);
-  // if (!loading) {
-  //   return (
-  //     <Image
-  //       fallbackSrc={thumbnail!}
-  //       className={classNames(
-  //         "w-full",
-  //         "h-full",
-  //         "bg-cover",
-  //         "transition-all",
-  //         "duration-50",
-  //         {
-  //           "blur-sm": loading,
-  //         }
-  //       )}
-  //       src={url}
-  //       onLoad={() => {
-  //         setLoading(false);
-  //       }}
-  //       onError={() => {
-  //         setLoading(false);
-  //       }}
-  //     />
-  //   );
-  // }
+  const patchStyle = useMemo(() => {
+    const style: CSSProperties = {
+      width: w,
+      height: h,
+      aspectRatio: `${w} / ${h}`,
+    };
+    if (w > h && w > 200) {
+      Object.assign(style, {
+        width: 200,
+        height: "auto",
+      });
+    }
+    if (h > w && h > 200) {
+      Object.assign(style, {
+        width: "auto",
+        height: 200,
+      });
+    }
+    return style;
+  }, []);
   return (
     <Image
+      style={{
+        ...patchStyle,
+      }}
       fallbackSrc={thumbnail!}
       className={classNames(
         "w-full",
