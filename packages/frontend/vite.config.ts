5932e9f9d946709663e3e49feb66306a79fe0d11
plh97 <pengliheng111@gmail.com>
2024-11-11 21:57:23 +0800

    fix: remove unused code change

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -110,15 +110,15 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://127.0.0.1:8080",
-        // target: "https://47.130.0.227:8080",
+        // target: "https://127.0.0.1:8080",
+        target: "https://47.130.0.227:8080",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
       "/chat": {
-        target: "wss://127.0.0.1:8080",
-        // target: "ws://47.130.0.227:8080",
+        // target: "wss://127.0.0.1:8080",
+        target: "wss://47.130.0.227:8080",
         changeOrigin: true,
         secure: false,
       },
