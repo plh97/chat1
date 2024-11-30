da584a448ce9b182a522d8d3d02e8bf00abfc70a
plh97 <pengliheng111@gmail.com>
2024-11-30 15:41:27 +0800

    feat: support ssl and http/ws

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -2,6 +2,6 @@ export const isDev = import.meta.env.MODE.toLocaleLowerCase() === "development";
 
 // wss://47.129.250.141:8081/ws
 // //47.129.250.141:8080/ws
-export const wsUrl = import.meta.env.VITE_WS_URL || "http://47.129.250.141:8081/ws";
+export const wsUrl = import.meta.env.VITE_WS_URL || "https://47.129.250.141:8082/ws";
 
 export const apiUrl = import.meta.env.VITE_API_URL || "/api";
