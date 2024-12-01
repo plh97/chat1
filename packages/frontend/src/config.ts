1ba5d8a50e85a9420713a3dc89b7e84ef04afa8b
plh97 <pengliheng111@gmail.com>
2024-12-01 14:34:02 +0800

    feat: support ssl and http/ws

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -3,6 +3,6 @@ export const isDev = import.meta.env.MODE.toLocaleLowerCase() === "development";
 // wss://3.0.58.48:8081/ws
 // //3.0.58.48:8080/ws
 export const wsUrl =
-  import.meta.env.VITE_WS_URL || isDev ? "/ws" : "https://3.0.58.48:8082/ws";
+  import.meta.env.VITE_WS_URL || isDev ? "/ws" : "ws://3.0.58.48/ws";
 
 export const apiUrl = import.meta.env.VITE_API_URL || "/api";
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -111,15 +111,15 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://localhost",
-        target: "https://3.0.58.48",
+        target: "http://localhost",
+        // target: "https://3.0.58.48",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
       "/ws": {
-        // target: "wss://localhost",
-        target: "wss://3.0.58.48",
+        target: "ws://localhost",
+        // target: "wss://3.0.58.48",
         changeOrigin: true,
         secure: false,
       },
